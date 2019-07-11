d562b21dc9fe2f1d6f024f7dec21ac7fc5a06983
Jason Prince <jasoncprince@gmail.com>
2019-07-11 16:23:37 -0600

    styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import "./App.css";
 import DatePicker from "./DatePicker";
 import Explanation from "./Explanation";
-// import DatePicker from './DatePicker'
+import Title from "./Title";
+import { Container, Image, Segment, Modal } from "semantic-ui-react";
 
 function App() {
-  const [pictures, setPictures] = useState('');
-  const [datePicker, setDatePicker] = useState('');
-  const [media, setMedia] = useState('');
-  const [explanation, setExplanation] = useState('');
+  const [pictures, setPictures] = useState("");
+  const [datePicker, setDatePicker] = useState("");
+  const [media, setMedia] = useState("");
+  const [explanation, setExplanation] = useState("");
+  const [title, setTitle] = useState("");
 
   console.log(datePicker);
   useEffect(() => {
     axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=jgD7VjRtm7bd4pN1T7Nid7FKA5Vcu9gbhbB3EIqk&date=${datePicker}`)
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=jgD7VjRtm7bd4pN1T7Nid7FKA5Vcu9gbhbB3EIqk&date=${datePicker}`
+      )
       .then(pic => {
         console.log(pic);
         if (pic.status === "error") {
@@ -23,44 +27,46 @@ function App() {
         setMedia(pic.data.media_type);
         setPictures(pic.data.url);
         setExplanation(pic.data.explanation);
+        setTitle(pic.data.title);
       })
       .catch(err => console.log("noooo"));
+  }, [datePicker]);
 
-    }, [datePicker]);
-    
-    return (
+  return (
+    <Container>
       <div>
-      <div className="flex flex-wrap justify-center">
-        <div className="flex ">
-          <DatePicker datePicker={setDatePicker}  />
+        <DatePicker datePicker={setDatePicker} />
+      </div>
+      <Segment>
+        {media === "video" ? (
+          <div>
+            <iframe
+              id="inlineFrameExample"
+              title={title}
+              src={pictures}
+              size="small"
+              floated="left"
+              href={pictures}
+            />
+          </div>
+        ) : (
+          <Modal
+            size="fullscreen"
+            trigger={
+              <Image alt={title} src={pictures} size="small" floated="left" />
+            }
+          >
+            <Modal.Content>
+              <Image alt={title} src={pictures} />
+            </Modal.Content>
+          </Modal>
+        )}
+        <div>
+          <Title title={title} />
+          <Explanation explanation={explanation} />
         </div>
-      </div>
-      <div className="flex">
-      {media === "video" ? 
-      <div className="w-1/2">
-        <iframe 
-          id="inlineFrameExample"
-          title="Inline Frame Example"
-          width="300"
-          height="200"
-          src={pictures}>
-        </iframe>
-      </div> 
-      :
-      <div className="w-1/2">
-        <img 
-          alt="nasa pictures and videos"
-          src={pictures}
-          width="300"
-          height="200">
-        </img>
-      </div>
-      }
-      <div className="w-1/2">
-       <Explanation explanation={explanation}/>
-      </div>
-      </div>
-      </div>
+      </Segment>
+    </Container>
   );
 }
 
